5180fc825b7a1089237447ce3715dc03fdd2fcee
wisdom-jessey <wisdomjessey5@gmail.com>
2022-09-08 17:47:03 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,26 @@
+#!/bin/bash
+#include <stdio.h>
+
+
+
+/**
+ *
+ * * main - Printing in C
+ *
+ * *
+ *
+ * * Return: Always 0 (Successful)
+ *
+ * */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual
+
+			puzzle");
+
+	return (0);
+
+}
