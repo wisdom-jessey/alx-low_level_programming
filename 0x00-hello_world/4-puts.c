aa3c800f66fd043947db8fff65a8a67ca7ab287c
wisdom-jessey <wisdomjessey5@gmail.com>
2022-09-08 18:11:25 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *
  * * main - Printing in C
@@ -12,12 +13,7 @@
 int main(void)
 
 {
-
-	puts("\"Programming is like building a multilingual
-
-			puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
-
 }
-gcc -Werror
