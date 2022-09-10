4a5eee331c6cf04c51f12370fb96ef14ac834e80
wisdom-jessey <wisdomjessey5@gmail.com>
2022-09-09 18:35:15 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,17 +5,14 @@
  *
  * Return: Always (Success)
  */
-
 int main(void)
-
 {
 char c;
-
 for (c = 'a'; c <= 'z'; c++)					
 {
 if (c != 'e' && c != 'q')
 {
-putchar(c);
+	putchar(c);
 }
 }
 putchar('\n');
