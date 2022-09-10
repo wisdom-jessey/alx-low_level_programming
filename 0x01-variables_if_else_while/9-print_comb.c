89cd0e47db690143c14dc879675e32874388f6a7
wisdom-jessey <wisdomjessey5@gmail.com>
2022-09-09 20:12:31 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,18 +10,14 @@
 int main(void)
 
 {
-
-int c;
-
-for (c = 48; c <= 57; c++)
-
-{
-	putchar(c);
-	if (c != 57)
+	int c;
+	for (c = 48; c <= 57; c++)
 	{
-		putchar(',');																putchar(' ');															}
-
-}
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');																putchar(' ');															}
+	}
 putchar('\n');
 return (0);
 }
