df4ff0ada4b59c7c3f5a14f2ad923662538d15fa
wisdom-jessey <wisdomjessey5@gmail.com>
2022-09-14 19:41:59 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,9 +12,9 @@ int print_sign(int n)
 {
 
 	if (n > 0)
+
 	{
 		_putchar('+');
-	
 		return (1);
 	}
 	else if (n < 0)
@@ -28,4 +28,3 @@ int print_sign(int n)
 		return (0);
 	}
 }
-M-x delete-trailing-whitespace
