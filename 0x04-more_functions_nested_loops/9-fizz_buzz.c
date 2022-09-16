5fb391901f9de36a3ad1fab1af0388f3aa608458
wisdom-jessey <wisdomjessey5@gmail.com>
2022-09-16 04:14:29 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,35 +9,35 @@
 int main(void)
 
 {
-	int num = 1;
+int num = 1;
 
-	while (num++ < 100)
+while (num++ < 100)
+{
+	if ((num % 3 == 0) && (num % 5 == 0))
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
+		printf("FizzBuzz ");
+	}
+	else if ((num % 3) == 0)
+	{
+		printf("Fizz ");
+	}
+	else if ((num % 5) == 0)
+	{
+		if (num != 100)
 		{
-			printf("FizzBuzz ");
-		}
-		else if ((num % 3) == 0)
-		{
-			printf("Fizz ");
-		}
-		else if ((num % 5) == 0)
-		{
-			if (num != 100)
-			{
-				printf("Buzz ");
-			}
-			else
-			{
-				printf("Buzz");
-			}
+			printf("Buzz ");
 		}
 		else
 		{
-			printf("%d ", num);
+			printf("Buzz");
 		}
 	}
-	printf("\n");
+	else
+	{
+		printf("%d ", num);
+	}
+}
+printf("\n");
 
-	return (0);
+return (0);
 }
