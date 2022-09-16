760f008343e0ab59ee97aeb6599c56fa21682d49
wisdom-jessey <wisdomjessey5@gmail.com>
2022-09-15 18:28:58 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * main - tests function that prints if integer is positive or negative
- *
  * Return: 0
  */
 
@@ -14,5 +13,4 @@ i = 0;
 positive_or_negative(i);
 
 return (0);
-
 }
