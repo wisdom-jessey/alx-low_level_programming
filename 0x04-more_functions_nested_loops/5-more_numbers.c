c42e25820b51e5654aa1309ab831d39fb0f2c560
wisdom-jessey <wisdomjessey5@gmail.com>
2022-09-15 20:25:06 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,16 +12,16 @@
 void more_numbers(void)
 
 {
-		int n1, n2;
+	int n1, n2;
 
-		for (n1 = 0; n1 < 10; n1++)
+	for (n1 = 0; n1 < 10; n1++)
+	{
+		for (n2 = 0; n2 <= 14; n2++)
 		{
-			for (n2 = 0; n2 <= 14; n2++)
-			{
-				if (n2 > 9)
-					putchar((n2 / 10) + '0');
-			}
-			putchar((n2 % 10) + '0');
+			if (n2 > 9)
+				putchar((n2 / 10) + '0');
 		}
-		putchar(10);
+		putchar((n2 % 10) + '0');
+	}
+	putchar(10);
 }
