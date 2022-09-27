3b922f3caa81060688b3b3e1c48552f81472192a
wisdom-jessey <wisdomjessey5@gmail.com>
2022-09-26 18:41:32 -0700

    0-memset.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,9 +2,6 @@
 
 #define MAIN_H
 
-#include <stdio.h>
-
-
 int _putchar(char c);                                                                                                              
 
 void *_memset(void *s, int c, size_t n);                                                                                           
