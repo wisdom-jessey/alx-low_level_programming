722650d54105e9517226cac509fa9c6fc200d8d3
wisdom-jessey <wisdomjessey5@gmail.com>
2022-11-02 07:22:16 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
-#include <sys/types>
+#include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
 
@@ -14,4 +14,4 @@ int create_file(const char *filename, char *text_content);
 int _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* MAIN_H */
+#endif
